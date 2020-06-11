0624763d4bad9a3925ce12fea1bb8ba31dd424e1
Jan Salvador Sebastian <sebastian@mclinica.com>
2020-06-12 01:12:38 +0800

    Initial Commit

---
+++ MVVM-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-Demo
+//
+//  Created by Jan Salvador Sebastian on 12/06/2020.
+//  Copyright © 2020 Jan Salvador Sebastian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
